c8b11a7c55f8429bf3b8a1b894f69d44cdf78ba6
guilhermeforprojeto <guylhermesantos@hotmail.com>
2022-03-25 10:53:19 -0300

    Array dentro do estado usando .map para rendereizar tudo com uma linha apenas

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-
 class App extends Component {
   state = { 
-    name: 'Guilherme Santana',
-    counter: 0
+    posts: [
+      {
+        id: 1,
+        title: 'O Titulo 1',
+        body: 'o corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O Titulo 2',
+        body: 'o corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O Titulo 3',
+        body: 'o corpo 3'
+      },  
+    ]
   };
-
-  handlePClick = () => {
-    this.setState({ name: 'Gui'});
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter + 1})
-  }
-
   render(){
-    const { name, counter} = this.state;
-
+    const { posts } = this.state;
     return (  
           <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p onClick={this.handlePClick}>
-              {name} {counter}
-            </p>
-            <a
-            onClick={this.handleAClick}
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Este é o link
-            </a>
-          </header>
+            {posts.map(post => (
+            <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            </div>
+            ))}
           </div>
       );
     }
   }
-
-
 export default App;
